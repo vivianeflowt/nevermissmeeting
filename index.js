a5a32f4a53b6d1e7b30fbad6e3d66beefddba10e
vivianeflowt <vivianeflowt@gmail.com>
2020-10-12 14:51:14 -0300

    codigo inicial

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+ * never miss a meeting :
+ * Authors: Viviane Florido / Hugo Braga
+ * Licensed under the MIT license.
+ */
+
+'use strict'
+
+const TaskFactory = require('./src/task').TaskFactory
+
+//  Dev-Tools
+module.exports.dev = {
+    TaskFactory,
+}
---
+++ src/service.js
@@ -0,0 +1,65 @@
+const moment = require('moment')
+const sleep = require('system-sleep')
+const openLink = require('opn')
+const data = require('./data.json')
+
+// Lista de Tarefas
+var taskList = []
+
+//Tarefa
+// const task1 = {
+//   link: "https://trybe.zoom.us/j/5355209477",
+//   job: {
+//     hour: 17,
+//     minute: 35,
+//     seconds: 0,
+//   },
+//   done: false,
+// };
+
+const task2 = {
+    link: 'https://trybe.zoom.us/j/5355209477',
+    job: {
+        hour: 01,
+        minute: 33,
+        seconds: 0,
+    },
+    done: false,
+}
+
+// Adiciona Tarefa na Lista
+taskList.push(task2)
+
+// testa se já passou do horário
+const taskCheck = (task = {}) => {
+    let now = moment()
+    let taskTime = moment()
+    taskTime.hour(task.job.hour)
+    taskTime.minute(task.job.minute)
+    taskTime.seconds(task.job.seconds)
+    const deltaTime = taskTime.diff(now)
+    console.log('.' + deltaTime)
+    if (deltaTime <= 0) {
+        return true
+    }
+    return false
+}
+
+// Serviço
+const start = (tickDelay = 5000) => {
+    console.log('PID:' + process.pid)
+    while (true) {
+        taskList.forEach((task) => {
+            if (task.done == false) {
+                if (taskCheck(task)) {
+                    task.done = true
+                    openLink(task.link)
+                }
+            }
+        })
+        sleep(tickDelay)
+    }
+}
+
+//Inicia os Serviço
+start()
